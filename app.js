835ad1cff1fe9c059a0e586db10894ef290018a2
joln17 <33175263+joln17@users.noreply.github.com>
2019-09-20 18:52:43 +0200

    Change port

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
-const port = 1337;
+const port = 8333;
 
 const index = require('./routes/index');
 const hello = require('./routes/hello');
