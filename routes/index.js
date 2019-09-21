a453f794d8ac357790fdbee5ccc2383f82e37651
joln17 <33175263+joln17@users.noreply.github.com>
2019-09-21 10:03:32 +0200

    Added me text

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,14 @@ const router = express.Router();
 router.get('/', (req, res) => {
     const data = {
         data: {
-            msg: "Hello World"
+            text: `Jag heter Johnny och bor i den nordväst-skånska lilla staden Ängelholm.
+                Till vardags jobbar jag på prisjämförelse­sajten Prisjakt med att ta fram och
+                analysera data.
+
+                Vid sidan av jobbet så läser jag just nu kursen [jsramverk](https://jsramverk.me/)
+                som ingår i programmet [Webbprogrammering](https://www.bth.se/utbildning/program/distansutbildningar/webbprogrammering/)
+                vid Blekinge Tekniska Högskola. I övrigt på min fritid gillar jag att träna, främst
+                padel, cykling och löpning.`
         }
     };
 
