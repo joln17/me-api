fda7b7cc88f3ff345530e5ef397c1ab1dddd2ecd
joln17 <33175263+joln17@users.noreply.github.com>
2019-09-21 23:53:48 +0200

    Add reports to migration query

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,5 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(60) NOT NULL,
+    password VARCHAR(64) NOT NULL,
+    admin INTEGER NOT NULL,
     UNIQUE(email)
+);
+
+CREATE TABLE IF NOT EXISTS reports (
+    title VARCHAR(255) NOT NULL,
+    text TEXT NOT NULL,
+    UNIQUE(title)
 );
