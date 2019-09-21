2a8d10f85abd44a7cdcbb1d3ddca11ad30f20a74
joln17 <33175263+joln17@users.noreply.github.com>
2019-09-22 01:02:52 +0200

    Added register function

--- models/auth-model.js
+++ models/auth-model.js
@@ -1,16 +1,72 @@
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const sqlite3 = require('sqlite3').verbose();
 
+const saltRounds = 10;
 const secret = process.env.JWT_SECRET;
+const db = new sqlite3.Database('../db/db.sqlite');
 
 const authModel = {
+    register: function (req, res) {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        if (!email || !password || password.length < 8) {
+            return res.status(401).json({
+                error: {
+                    status: 401,
+                    message: "Email or password not valid"
+                }
+            });
+        }
+
+        bcrypt.hash(password, saltRounds, (err, hash) => {
+            if (err) {
+                return res.status(500).json({
+                    error: {
+                        status: 500,
+                        message: "Bcrypt error"
+                    }
+                });
+            }
+
+            db.run("INSERT INTO users (email, password, admin) VALUES (?, ?, ?)",
+                email,
+                hash,
+                0,
+                (err) => {
+                    if (err) {
+                        return res.status(500).json({
+                            error: {
+                                status: 500,
+                                message: "Database error"
+                            }
+                        });
+                    }
+
+                    const payload = { email: email };
+                    const token = jwt.sign(payload, secret, { expiresIn: '1h'});
+
+                    return res.status(201).json({
+                        data: {
+                            status: 201,
+                            message: "User successfully registered",
+                            user: payload,
+                            token: token
+                        }
+                    });
+                });
+        });
+    },
+
     login: function (req, res) {
         const payload = { email: req.body.email };
         const token = jwt.sign(payload, secret, { expiresIn: '1h'});
 
-        return res.json({
+        return res.status(200).json({
             data: {
-                //type: "success",
-                //message: "User logged in",
+                status: 200,
+                message: "User logged in",
                 user: payload,
                 token: token
             }
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const auth = require('../models/auth-model');
 
 const router = express.Router();
 
+router.post('/register', (req, res) => auth.register(req, res));
 router.post('/login', (req, res) => auth.login(req, res));
 
 module.exports = router;
