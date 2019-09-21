d6b85f7ca2c0f93c80869f99199eabe1b2df0d73
joln17 <33175263+joln17@users.noreply.github.com>
2019-09-21 10:30:21 +0200

    Update me text 1

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+/*eslint max-len: ["error", { "ignoreTemplateLiterals": true }]*/
+
 router.get('/', (req, res) => {
     const data = {
-        data: {
-            text: `Jag heter Johnny och bor i den nordväst-skånska lilla staden Ängelholm.
-                Till vardags jobbar jag på prisjämförelse­sajten Prisjakt med att ta fram och
-                analysera data.
+        text:
+`Jag heter Johnny och bor i den nordväst-skånska lilla staden Ängelholm. Till vardags jobbar jag på prisjämförelse­sajten Prisjakt med att ta fram och analysera data.
 
-                Vid sidan av jobbet så läser jag just nu kursen [jsramverk](https://jsramverk.me/)
-                som ingår i programmet [Webbprogrammering](https://www.bth.se/utbildning/program/distansutbildningar/webbprogrammering/)
-                vid Blekinge Tekniska Högskola. I övrigt på min fritid gillar jag att träna, främst
-                padel, cykling och löpning.`
-        }
+Vid sidan av jobbet så läser jag just nu kursen [jsramverk](https://jsramverk.me/) som ingår i programmet [Webbprogrammering](https://www.bth.se/utbildning/program/distansutbildningar/webbprogrammering/) vid Blekinge Tekniska Högskola. I övrigt på min fritid gillar jag att träna, främst padel, cykling och löpning.`
     };
 
     res.json(data);
