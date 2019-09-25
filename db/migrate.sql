b018c6478d9c81f4092aa5b08fd4f700604d3f75
joln17 <33175263+joln17@users.noreply.github.com>
2019-09-25 23:02:18 +0200

    Update for kmom03

--- db/migrate.sql
+++ db/migrate.sql
@@ -1,11 +1,15 @@
 CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(64) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    name VARCHAR(255),
+    birthdate DATE,
     admin INTEGER NOT NULL,
     UNIQUE(email)
 );
 
 CREATE TABLE IF NOT EXISTS reports (
+    id INTEGER PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     text TEXT NOT NULL,
     UNIQUE(title)
